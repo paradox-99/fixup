20bcaaf1857f1bfdb032ac43c331e6772f3949e7
Radwanul Islam Nayeem <103718355+paradox-99@users.noreply.github.com>
2023-03-03 17:41:48 +0600

    Code for AOOP project

---
+++ src/main/java/sourcefile/App.java
@@ -0,0 +1,43 @@
+package sourcefile;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("homePage"));
+        //scene.setFill(Color.TRANSPARENT);
+        // taskbar icon image
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("images/taskBarIcon.png")));
+        stage.setTitle("FiX Up"); // taskbar title
+        //stage.initStyle(StageStyle.TRANSPARENT);
+        //stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/sourcefile/HomePageController.java
@@ -0,0 +1,12 @@
+package sourcefile;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class HomePageController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+       // App.setRoot("secondary");
+    }
+}
