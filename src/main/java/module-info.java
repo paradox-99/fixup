2e1f451fb0bf6b76f9d69f800cdc447e1833aafd
Radwanul Islam Nayeem <103718355+paradox-99@users.noreply.github.com>
2023-04-02 17:11:27 +0600

    To pull.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module sourcefile {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.web;
 
     opens sourcefile to javafx.fxml;
     exports sourcefile;
